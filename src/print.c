283200b83c35a8e781ce485677e54892e70019d7
Tiisetso Murray <timurray@student.hive.fi>
2026-01-24 13:29:12 +0200

    fix: mutex functions.

--- src/print.c
+++ src/print.c
@@ -6,7 +6,7 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 11:52:43 by timurray          #+#    #+#             */
-/*   Updated: 2026/01/21 13:28:20 by timurray         ###   ########.fr       */
+/*   Updated: 2026/01/24 13:27:48 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void mprint(t_status status, t_philo *philo)
 	if (philo->full)
 		return ;
 
-	fork_lock(&philo->table->write_lock);
+	mx_lock(&philo->table->write_lock);
 	if ((status == L_FORK || status == R_FORK) && !simulation_finished(philo->table))
 		printf("%u %u has taken a fork\n", elapsed - philo->table->time_start, philo->i);
 	else if ((status == EAT && !simulation_finished(philo->table)))
@@ -32,5 +32,5 @@ void mprint(t_status status, t_philo *philo)
 	else if (status == DIE)
 		printf("%u %u died\n", elapsed - philo->table->time_start, philo->i);
 
-	fork_unlock(&philo->table->write_lock);
+	mx_unlock(&philo->table->write_lock);
 }
