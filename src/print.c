ae6cbd023f7c74fcc0c45e9f85b769a5255224c1
Tiisetso Murray <timurray@student.hive.fi>
2026-01-30 22:24:59 +0200

    fix: over print.

--- src/print.c
+++ src/print.c
@@ -6,7 +6,7 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 11:52:43 by timurray          #+#    #+#             */
-/*   Updated: 2026/01/30 20:55:21 by timurray         ###   ########.fr       */
+/*   Updated: 2026/01/30 22:22:43 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ void	mx_print(char *text, t_philo *philo)
 		return ;
 	if (end_table(philo->table))
 		return ;
-	// mx_lock(&philo->table->write_lock);
+	mx_lock(&philo->table->write_lock);
 	elapsed = get_time_ms() - philo->table->time_start;
 	if (end_table(philo->table))
 	{
-		// mx_unlock(&philo->table->write_lock);
+		mx_unlock(&philo->table->write_lock);
 		return ;
 	}
-	mx_lock(&philo->table->write_lock);
+	// mx_lock(&philo->table->write_lock);
 	printf("%lu %lu %s\n", elapsed, philo->i, text);
 	mx_unlock(&philo->table->write_lock);
 }
